bda5748ef87175dc6a4442d1509b4cbd64208c62
aka-andiska <aka.afenandiska@tampinestechnology.com>
2017-01-12 11:33:19 +0700

    setting products/urls.py, create module productsapp and setting urls.py and models.py

--- productsapp/models.py
+++ productsapp/models.py
@@ -1,3 +1,9 @@
+from __future__ import unicode_literals
+
 from django.db import models
 
-# Create your models here.
+class Products(models.Model):
+    title = models.CharField(max_length = 255)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add = True)
+    published_date = models.DateTimeField(auto_now = True)
